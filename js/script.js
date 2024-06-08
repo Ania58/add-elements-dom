555a9bde814c9fc8f83ac0682f605dce0470e419
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2024-06-08 21:37:23 +0200

    some changes en JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
  // Aquí tu código
+
+const list = prompt('Create your list');
+const addItems = document.getElementById('lista');
+const clickToAdd = document.getElementById('agregar');
+
+clickToAdd.addEventListener('click', function(){
+    const list = prompt('Create your list');
+    if (list) {
+        const elementList = document.createElement('li');
+        elementList.textContent = list;
+        addItems.appendChild(elementList);
+      }
+    agregar.innerText = 'Hola, crea tu propia lista de elementos ' + list + ' y agregala aqui.';
+})
